fd1f389fe6b479c6e18c8ed5e8db1132c3abba5c
Conrad Hansen-Quartey <Chansenq@gmail.com>
2024-08-28 10:42:33 -0400

    feat: formbuilder frontend scaffold
    Adding scaffold for the front end of the formbuilder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+// import 'axios';
+// import Axios from 'axios';
+import axios from 'axios';
+
+const apiCall = () => {
+  axios.get('http://localhost:5000').then((data) => {
+    // this console.log will be in our frontend console
+    console.log(data)
+  })
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={apiCall}>Make API call</button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
