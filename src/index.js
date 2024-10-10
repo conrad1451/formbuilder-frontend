49cab4422cecd943a45497080b1a143446a58641
Conrad Hansen-Quartey <Chansenq@gmail.com>
2024-10-10 10:47:45 -0400

    redux test

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import AppAlt from './AppAlt';
+import store from './app/store';
+import { Provider } from 'react-redux';
+import * as serviceWorker from './serviceWorker';
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <AppAlt />
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
