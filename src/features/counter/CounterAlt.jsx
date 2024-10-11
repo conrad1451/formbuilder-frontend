213e503048d20eea3e3b85c7d25551b30216d7c6
Conrad Hansen-Quartey <Chansenq@gmail.com>
2024-10-11 03:55:09 -0400

    refactor: created two components called from main one

--- src/features/counter/CounterAlt.jsx
+++ src/features/counter/CounterAlt.jsx
@@ -9,64 +9,135 @@ import {
 } from './counterSlice';
 import styles from './Counter.module.css';
 
-export function Counter() {
-  const count = useSelector(selectCount);
-  const dispatch = useDispatch();
-  const [incrementAmount, setIncrementAmount] = useState('2');
+function FirstCounter()  {
+    const count = useSelector(selectCount);
+    const mySecondVar = useSelector(selectCount);
+    const dispatch = useDispatch();
+    const [incrementAmount, setIncrementAmount] = useState('2');
+  
+    let bro = Array(3).fill().map(Math.random);
+  
+    return (
+      <div>
+          <p>My second variable is {mySecondVar}</p>
+        <div className={styles.row}>
+          <button
+            className={styles.button}
+            aria-label="Increment value"
+            onClick={() => dispatch(increment())}
+          >
+            +
+          </button>
+          <span className={styles.value}>{count}</span>
+          <button
+            className={styles.button}
+            aria-label="Decrement value"
+            onClick={() => dispatch(decrement())}
+          >
+            -
+          </button>
+        </div>
+        <div> 
+            {/* source :https://stackoverflow.com/questions/18499935/idiom-for-repeat-n-times */}
+          {Array(5).fill().map(( ) => {
+                  return (<><span>Bro</span><text> | </text></>)
+              })} Bro 
+          <ul> 
+              {Array(count).fill().map(( ) => {
+                  return (<li>Hi</li>)
+              })} 
+          </ul>
+        </div>
+        <div className={styles.row}>
+          <input
+            className={styles.textbox}
+            aria-label="Set increment amount"
+            value={incrementAmount}
+            onChange={e => setIncrementAmount(e.target.value)}
+          />
+          <button
+            className={styles.button}
+            onClick={() =>
+              dispatch(incrementByAmount(Number(incrementAmount) || 0))
+            }
+          >
+            Add Amount
+          </button>
+          <button
+            className={styles.asyncButton}
+            onClick={() => dispatch(incrementAsync(Number(incrementAmount) || 0))}
+          >
+            Add Async
+          </button>
+        </div>
+      </div>
+    );
+  }
 
-  let bro = Array(3).fill().map(Math.random);
-
-  return (
-    <div>
-      <div className={styles.row}>
-        <button
-          className={styles.button}
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          +
-        </button>
-        <span className={styles.value}>{count}</span>
-        <button
-          className={styles.button}
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          -
-        </button>
+function SecondCounter()  {
+    const count = useSelector(selectCount);
+    const mySecondVar = useSelector(selectCount);
+    const dispatch = useDispatch();
+    const [incrementAmount, setIncrementAmount] = useState('2');
+  
+    let bro = Array(3).fill().map(Math.random);
+  
+    return (
+      <div>
+          <p>My second variable is {mySecondVar}</p>
+        <div className={styles.row}>
+          <button
+            className={styles.button}
+            aria-label="Increment value"
+            onClick={() => dispatch(increment())}
+          >
+            +
+          </button>
+          <span className={styles.value}>{count}</span>
+          <button
+            className={styles.button}
+            aria-label="Decrement value"
+            onClick={() => dispatch(decrement())}
+          >
+            -
+          </button>
+        </div>
+        <div> 
+          {Array(5).fill().map(( ) => {
+                  return (<><span>Bro</span><text> | </text></>)
+              })} Bro 
+          <ul> 
+              {Array(count).fill().map(( ) => {
+                  return (<li>Hi</li>)
+              })} 
+          </ul>
+        </div>
+        <div className={styles.row}>
+          <input
+            className={styles.textbox}
+            aria-label="Set increment amount"
+            value={incrementAmount}
+            onChange={e => setIncrementAmount(e.target.value)}
+          />
+          <button
+            className={styles.button}
+            onClick={() =>
+              dispatch(incrementByAmount(Number(incrementAmount) || 0))
+            }
+          >
+            Add Amount
+          </button>
+          <button
+            className={styles.asyncButton}
+            onClick={() => dispatch(incrementAsync(Number(incrementAmount) || 0))}
+          >
+            Add Async
+          </button>
+        </div>
       </div>
-      <div> 
-        {Array(count-1).fill().map(( ) => {
-                return (<><span>Bro</span><text> | </text></>)
-            })} Bro 
-        <ul> 
-            {Array(count).fill().map(( ) => {
-                return (<li>Hi</li>)
-            })} 
-        </ul>
-      </div>
-      <div className={styles.row}>
-        <input
-          className={styles.textbox}
-          aria-label="Set increment amount"
-          value={incrementAmount}
-          onChange={e => setIncrementAmount(e.target.value)}
-        />
-        <button
-          className={styles.button}
-          onClick={() =>
-            dispatch(incrementByAmount(Number(incrementAmount) || 0))
-          }
-        >
-          Add Amount
-        </button>
-        <button
-          className={styles.asyncButton}
-          onClick={() => dispatch(incrementAsync(Number(incrementAmount) || 0))}
-        >
-          Add Async
-        </button>
-      </div>
-    </div>
-  );
+    );
+  }
+export function Counter() 
+{
+    return(<FirstCounter/>)
 }
