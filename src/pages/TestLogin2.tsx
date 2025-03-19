b9a1fcc633f826b7be37ace93f44feef32572215
Conrad Hansen-Quartey <Chansenq@gmail.com>
2025-03-19 13:02:22 -0400

    feat: login endpoits set in variables

--- src/pages/TestLogin2.tsx
+++ src/pages/TestLogin2.tsx
@@ -1,7 +1,33 @@
 import React from "react"
 import { useForm, SubmitHandler } from "react-hook-form"
 
+/**
+ * AI suggestion from google
+ * // .env file
+REACT_APP_API_KEY=mysecretkey
 
+// App.js
+import React from 'react';
+import { Env } from 'react-dotenv';
+
+function App() {
+  return (
+    <div>
+      <h1>{process.env.REACT_APP_API_KEY}</h1>
+    </div>
+  );
+}
+
+export default App;
+ * 
+ */
+// Module '"react-dotenv"' has no exported member 'Env'. Did you mean to use 'import Env from "react-dotenv"' instead?ts(2614)
+// Module '"react-dotenv"' has no exported member 'env'. Did you mean to use 'import env from "react-dotenv"' instead?ts(2614)
+
+// import env from "react-dotenv";
+// import env from 'dotenv';
+
+// env.config();
 type FormValues = {
   firstName: string
   lastName: string
@@ -15,6 +41,15 @@ type FormValues2 = {
   }
 
 
+  // const LOGIN_SERVER = String(process.env.REACT_APP_LOGIN_SERVER_1);
+
+  // const LOGIN_SERVER = String(process.env.REACT_APP_LOGIN_SERVER_1);
+
+const LOGIN_SERVER_1="http://localhost:5000/login"
+const LOGIN_SERVER_2="https://formbuilder-server-6soo.onrender.com/login"
+
+const LOGIN_SERVER = LOGIN_SERVER_1
+
 export default function MyLogin() {
   const { register, handleSubmit } = useForm<FormValues2>()
 //   const onSubmit: SubmitHandler<FormValues2> = (data) => console.log(data)
@@ -26,7 +61,13 @@ const onSubmit: SubmitHandler<FormValues2> = async (data: FormValues2) => {
         // e.preventDefault();
     
         // Send a POST request to the specified endpoint
-        const response = await fetch( "http://localhost:5000/login", {
+        // env.REACT_APP_LOGIN_SERVER
+                // env.REACT_APP_LOGIN_SERVER
+                const response = await fetch(LOGIN_SERVER, {
+        // const response = await fetch(env.REACT_APP_LOGIN_SERVER_1, {
+        // const response = await fetch(env.REACT_APP_LOGIN_SERVER_2, {
+        // const response = await fetch( "https://localhost:5000/login", {
+        // const response = await fetch( "http://localhost:5000/login", {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -56,8 +97,9 @@ const onSubmit: SubmitHandler<FormValues2> = async (data: FormValues2) => {
       <input type="text" {...register("username")} />
       <input type="email" {...register("email")} />
       <input type="password" {...register("password")} />
+      <input type="submit"/>
 
-      <input type="submit" />
+      {/* <input type="submit" title="Log in"/> */}
     </form>
   )
 }
