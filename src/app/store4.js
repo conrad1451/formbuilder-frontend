f9dfbbb11644f01cadb00b4799413e8d143e1df9
Conrad Hansen-Quartey <Chansenq@gmail.com>
2025-04-01 15:52:54 -0400

    Delete src directory

--- src/app/store4.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import compIDReducer from '../features/counter/questionComponentSlice';
-
-export default configureStore({
-  reducer: {
-    compID: compIDReducer,
-  },
-});
